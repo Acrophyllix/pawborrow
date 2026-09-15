470d2ce7cd9d168bdb3f4e9c765adeab02bbba99
juntaror <qynmiura@tip.edu.ph>
2026-09-16 03:44:31 +0800

    bug fix

--- packages/api/src/lib/likedpets/useLikedPets.ts
+++ packages/api/src/lib/likedpets/useLikedPets.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLikedPets } from "./likedPet";
 
-export function useLikedPets() {
+export function useLikedPets(enabled = true) {
   return useQuery({
     queryKey: ["liked-pets"],
     queryFn: getLikedPets,
+    enabled,
   });
 }
